9608c33ab3f11c9c70337f06d9cbf4a32b1f118c
Frost Ming <me@frostming.com>
2024-09-25 13:30:27 +0800

    fix: replace links
    Signed-off-by: Frost Ming <me@frostming.com>

--- src/lib.ts
+++ src/lib.ts
@@ -1,5 +1,11 @@
 export function fixImagesAndLinks(html: string): string {
-    return html.replaceAll(/<img [^>]+? data-actualsrc="([^>]+?)"[^>]+?(\/?>)/g, (match, p1, p2) => {
+    let result = html.replaceAll(/<img [^>]+? data-actualsrc="([^>]+?)"[^>]+?(\/?>)/g, (match, p1, p2) => {
         return `<img src="${p1}"${p2}`;
     });
+    result = result.replaceAll(/href="(https:\/\/link\.zhihu\.com\/.+?)"/g, (match, p1) => {
+        const url = new URL(p1);
+        const target = decodeURIComponent(url.searchParams.get('target') || '');
+        return `href="${target}"`;
+    });
+    return result;
 }
