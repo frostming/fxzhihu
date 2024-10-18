85c6c1f38b3cc8db68f29a7b2a5d869a8a34bc82
Asuka Minato <i@asukaminato.eu.org>
2024-10-18 11:22:39 +0900

    feat: Support answer link (#14)
    * feat: Update answer template to include author's headline and avatar URL
    
    - Add new properties to the Answer type for author's headline and avatar URL
    - Update the answer template to display the author's headline and avatar URL
    - Modify the answer function to include the author's headline and avatar URL in the rendered template
    
    * feat: Add support for answering specific questions
    
    adds support for answering specific questions by adding a new route handler for paths that match the pattern "/answer/{answerId}". The handler retrieves the answer based on the provided answer ID and returns it as a response with the appropriate content type.
    
    Fixes #12

--- src/answer.ts
+++ src/answer.ts
@@ -2,15 +2,18 @@ import { Question } from "./question";
 import { fixImagesAndLinks, renderTemplate } from "./lib";
 
 export type Answer = {
-    content: string;
-    excerpt: string;
-    author: {
-        name: string;
-    };
-    voteup_count: number;
-    comment_count: number;
-    question: Question;
-    created_time: number;
+	content: string;
+	excerpt: string;
+	author: {
+		name: string;
+		url: string;
+		headline: string;
+		avatar_url: string;
+	};
+	voteup_count: number;
+	comment_count: number;
+	question: Question;
+	created_time: number;
 }
 
 const template = renderTemplate`
@@ -45,7 +48,10 @@ const template = renderTemplate`
 <body style="max-width: 1000px; margin: 0 auto; padding: 0 1em 0 1em;">
     <header>
         <h1>${"title"}</h1>
-        <h2 rel="author">@${"author"}</h2>
+        <h2 rel="author">
+		<a href="${"author_url"}" target="_blank">@${"author"}</a>
+		</h2>
+		<p> ${"headline"} </p>
         <time datetime="${"created_time"}">发表于 ${"created_time_formatted"}</time>
         <p rel="stats"style="color: #999; font-size: 0.9em;">${"voteup_count"} 👍 / ${"comment_count"} 💬</p>
     </header>
@@ -85,5 +91,8 @@ export async function answer(id: string, redirect: boolean, env: Env): Promise<s
 			question: fixImagesAndLinks(data.question.detail),
 		}) : '',
 		redirect: redirect ? 'true' : 'false',
+		author_url: data.author.url.replace("api.", ""),
+		headline: data.author.headline,
+		avatar_url: data.author.avatar_url,
 	});
 }
